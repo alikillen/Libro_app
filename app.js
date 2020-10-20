04f190b05ed677127b857b6e6b6c108229e68a71
alikillen <alisoneme1994@gmail.com>
2020-10-20 11:55:14 +1000

    searchterms

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser")
 const cors = require("cors")
 const mongoose = require("mongoose")
 const bookRouter = require("./routes/book_routes")
+const api_helper = require('./API_helper')
 // import {v4 as uuidv4} from 'uuid'
 
 const port = 3000
@@ -39,6 +40,30 @@ app.set('view engine', 'handlebars');
 app.get('/', (req, res) => {
   res.render("home");
 })
+
+app.get("/search", (req, res)=>{
+	res.render("search")
+})
+
+app.get("/search", (req,res)=>{
+	api_helper.make_API_call('https://jsonplaceholder.typicode.com/todos/1')
+	.then(response => {
+			res.json(response)
+	})
+	.catch(error => {
+			res.send(error)
+	})
+})
+
+// app.get('/getAPIResponse', (req, res) => {
+// 	api_helper.make_API_call('https://jsonplaceholder.typicode.com/todos/1')
+// 	.then(response => {
+// 			res.json(response)
+// 	})
+// 	.catch(error => {
+// 			res.send(error)
+// 	})
+// })
 
 app.use("/books", bookRouter)
 
